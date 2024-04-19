7c79fafa4b2448409a119580f850e9ad7eb28d1d
adriDiazz <adriandiazmanzanares9@gmail.com>
2024-04-19 18:36:03 +0200

    helmet sefcurity added

--- transcription-service/src/main.ts
+++ transcription-service/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './utils/HttpExceptionFilter';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.enableCors({
     origin: '*',
   });
